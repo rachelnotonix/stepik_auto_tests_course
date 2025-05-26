748d8d9ad26129963f84b31e541cdf7e3bc69294
ifyod <ifyodrv@yandex.ru>
2025-05-26 11:41:20 +0300

    Switching from unicode to pytest. Adding the second test case for the other site.

--- test_registrations.py
+++ test_registrations.py
@@ -3,13 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.devtools.v136.page import bring_to_front
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import unittest
+import pytest
 import time, math, os
 
 
-class TestRegistration(unittest.TestCase):
+class TestRegistration:
 
-    def test_registration_all_required(self):
+    def test_registration_one_all_required(self):
 
         try:
             browser = webdriver.Chrome()
@@ -29,7 +29,33 @@ class TestRegistration(unittest.TestCase):
             self.button_submit.click()
 
             self.checking = browser.find_element(By.XPATH, "//h1").text
-            self.assertEqual(self.checking, "Congratulations! You have successfully registered!", "Should be the congratulations message.")
+            assert self.checking == "Congratulations! You have successfully registered!", "Should be the congratulations message."
+
+        finally:
+            time.sleep(10)
+            browser.quit()
+
+    def test_registration_two_all_required(self):
+
+        try:
+            browser = webdriver.Chrome()
+            self.link = "http://suninjuly.github.io/registration1.html"
+            browser.get(self.link)
+
+            self.first_name = browser.find_element(By.XPATH, "//div[label[contains(text(), 'First name*')]]//input")
+            self.first_name.send_keys("zalupa")
+
+            self.last_name = browser.find_element(By.XPATH, "//div[label[contains(text(), 'Last name*')]]//input")
+            self.last_name.send_keys("zalupovich")
+
+            self.email = browser.find_element(By.XPATH, "//div[label[contains(text(), 'Email*')]]//input")
+            self.email.send_keys("zalupa@com")
+
+            self.button_submit = browser.find_element(By.XPATH, "//button[@type='submit']")
+            self.button_submit.click()
+
+            self.checking = browser.find_element(By.XPATH, "//h1").text
+            assert self.checking == "Congratulations! You have successfully registered!", "Should be the congratulations message."
 
         finally:
             time.sleep(10)
@@ -86,4 +112,4 @@ class TestRegistration(unittest.TestCase):
     #         browser.quit()
 
 if __name__ == "__main__":
-    unittest.main()
+    pytest.main()
