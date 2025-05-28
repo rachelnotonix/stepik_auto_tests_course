4b7b5ab9ce79ff6cbb65b4ac723f8eb865ac6afc
ifyod <ifyodrv@yandex.ru>
2025-05-28 11:57:52 +0300

    Updating test for the new test case.

--- test_registrations.py
+++ test_registrations.py
@@ -32,7 +32,7 @@ class TestRegistration:
             assert self.checking == "Congratulations! You have successfully registered!", "Should be the congratulations message."
 
         finally:
-            time.sleep(10)
+            time.sleep(5)
             browser.quit()
 
     def test_registration_two_all_required(self):
@@ -58,39 +58,39 @@ class TestRegistration:
             assert self.checking == "Congratulations! You have successfully registered!", "Should be the congratulations message."
 
         finally:
+            time.sleep(5)
+            browser.quit()
+
+    def test_registration_all_fields(self):
+
+        try:
+            browser = webdriver.Chrome()
+            self.link = "http://suninjuly.github.io/registration1.html"
+            browser.get(self.link)
+
+            self.first_name = browser.find_element(By.XPATH, "//div[label[contains(text(), 'First name*')]]//input")
+            self.first_name.send_keys("123II9Z")
+
+            self.last_name = browser.find_element(By.XPATH, "//div[label[contains(text(), 'Last name*')]]//input")
+            self.last_name.send_keys("123TPNY")
+
+            self.email = browser.find_element(By.XPATH, "//div[label[contains(text(), 'Email*')]]//input")
+            self.email.send_keys("123LYFS")
+
+            self.phone = browser.find_element(By.XPATH, "//div[label[contains(text(), 'Phone')]]//input")
+            self.phone.send_keys("123YBAK")
+
+            self.address = browser.find_element(By.XPATH, "//div[label[contains(text(), 'Address')]]//input")
+            self.address.send_keys("123VY9W")
+
+            self.button_submit = browser.find_element(By.XPATH, "//button[@type='submit']")
+            self.button_submit.click()
+
+        finally:
+
             time.sleep(10)
             browser.quit()
 
-    # def test_registration_all_fields(self):
-    #
-    #     try:
-    #         browser = webdriver.Chrome()
-    #         self.link = "http://suninjuly.github.io/registration1.html"
-    #         browser.get(self.link)
-    #
-    #         self.first_name = browser.find_element(By.XPATH, "//div[label[contains(text(), 'First name*')]]//input")
-    #         self.first_name.send_keys("zalupa")
-    #
-    #         self.last_name = browser.find_element(By.XPATH, "//div[label[contains(text(), 'Last name*')]]//input")
-    #         self.last_name.send_keys("zalupovich")
-    #
-    #         self.email = browser.find_element(By.XPATH, "//div[label[contains(text(), 'Email*')]]//input")
-    #         self.email.send_keys("zalupa@com")
-    #
-    #         self.phone = browser.find_element(By.XPATH, "//div[label[contains(text(), 'Phone')]]//input")
-    #         self.phone.send_keys("12345")
-    #
-    #         self.address = browser.find_element(By.XPATH, "//div[label[contains(text(), 'Address')]]//input")
-    #         self.address.send_keys("zalupinsk")
-    #
-    #         self.button_submit = browser.find_element(By.XPATH, "//button[@type='submit']")
-    #         self.button_submit.click()
-    #
-    #     finally:
-    #
-    #         time.sleep(10)
-    #         browser.quit()
-    #
     # def test_registration_miss_one_required(self):
     #
     #     try:
